dac88fabd6c55cb0fa55e29f4d9e66ef2deb0f78
eda0717 <playgirl1209@icloud.com>
2020-12-22 00:07:17 +0800

    Update 2020-12-21T16:06:55.662Z

---
+++ assets/js/all.js
@@ -0,0 +1,23 @@
+"use strict";
+
+$(document).ready(function () {
+  //navbar滾動變色
+  $(window).scroll(function (e) {
+    if ($(window).scrollTop() <= 0) {
+      $('.navbar').addClass('bg-white');
+      $('.navbar').removeClass('bg-light');
+    } else {
+      $('.navbar').addClass('bg-light');
+      $('.navbar').removeClass('bg-white');
+    }
+  });
+  $('#load img').load(function () {
+    //圖片預設隱藏
+    $(this).hide(); //使用fadeIn特效
+
+    $(this).fadeIn('5000');
+  }); // $(body).scroll(function () {
+  //     $("p").slideToggle("slow");
+  // });
+});
+//# sourceMappingURL=all.js.map
